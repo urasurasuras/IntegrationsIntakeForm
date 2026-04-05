f727ff3fafceb55f36608595e7a86a78b2687f23
urasurasuras <uras.oran.htc@gmail.com>
2026-04-04 21:20:00 -0400

    Target time picker

--- IntegrationsIntakeForm/Models/IntegrationIntakeModel.cs
+++ IntegrationsIntakeForm/Models/IntegrationIntakeModel.cs
@@ -3,41 +3,31 @@ namespace IntegrationsIntakeForm.Models;
 // The full intake form state
 public class IntegrationIntakeModel
 {
-    // Integration Capabilities
-    public List<IntegrationFeature> Features { get; set; } = new List<IntegrationFeature>
-    {
-        new IntegrationFeature { Name = "Order Creation", IsEnabled = true },
-        new IntegrationFeature { Name = "Update", IsEnabled = true },
-        new IntegrationFeature { Name = "Deletion", IsEnabled = true }
-    };
-
-    // Global Account Mapping
-    public MappingDetail GlobalAccountMapping { get; set; } = new MappingDetail();
-
-    // Required ID Fields (with nested rules)
+    public List<Feature> Features { get; set; } = 
+    [
+        new Feature { Name = "Order Creation", IsEnabled = true },
+        new Feature { Name = "Update", IsEnabled = true },
+        new Feature { Name = "Deletion", IsEnabled = true }
+    ];
     public List<RequiredFieldModel> RequiredIdFields { get; set; } = new List<RequiredFieldModel>
     {
         new RequiredFieldModel { FieldName = "Account Number" },
         new RequiredFieldModel { FieldName = "Service Type" },
         new RequiredFieldModel { FieldName = "Vehicle Type" },
         new RequiredFieldModel { FieldName = "Package Type" }
-    };
-
-    // Pickup / Delivery Target Time Windows (Conditional Section)
-    public bool MapInboundTimeWindows { get; set; } = false;
-    public bool InboundDate_FromFile { get; set; } = true;
-    public bool InboundTime_FromFile { get; set; } = false;
-
-    // Package Itemization (Conditional Section)
-    public bool EnableItemization { get; set; } = false; // Starts unchecked based on picture
+    };    
+    // Time Windows Properties
+    public string PickupFromDateSource { get; set; } = "FromFile";
+    public string PickupFromTimeSource { get; set; } = "FromFile";
+    public string PickupToDateSource { get; set; } = "FromFile";
+    public string PickupToTimeSource { get; set; } = "FromFile";
+    public string DeliveryFromDateSource { get; set; } = "FromFile";
+    public string DeliveryFromTimeSource { get; set; } = "FromFile";
+    public string DeliveryToDateSource { get; set; } = "FromFile";
+    public string DeliveryToTimeSource { get; set; } = "FromFile";
+    
+    // Package Itemization Properties
+    public bool EnableItemization { get; set; } = false;
     public NoItemizationType NoItemizationSubOption { get; set; } = NoItemizationType.FromFileWithMapping;
-    public string ItemizedRefNoMapping { get; set; } = "";
-
-    // Misc Notes
-    public List<string> MiscNotes { get; set; } = new List<string>
-    {
-        "No need to have non-mappings",
-        "Be explicit with usage",
-        "Section off mapping w/ multiple layers"
-    };
+    public string ItemizedRefNoMapping { get; set; } = string.Empty;
 }
