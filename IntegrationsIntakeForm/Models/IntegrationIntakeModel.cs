6d6c81f3633feb79ac7de8103f972230358a549b
urasurasuras <uras.oran.htc@gmail.com>
2026-04-04 22:33:22 -0400

    Package type inherits from required field

--- IntegrationsIntakeForm/Models/IntegrationIntakeModel.cs
+++ IntegrationsIntakeForm/Models/IntegrationIntakeModel.cs
@@ -9,13 +9,13 @@ public class IntegrationIntakeModel
         new Feature { Name = "Update", IsEnabled = true },
         new Feature { Name = "Deletion", IsEnabled = true }
     ];
-    public List<RequiredFieldModel> RequiredIdFields { get; set; } = new List<RequiredFieldModel>
-    {
+    public List<RequiredFieldModel> RequiredIdFields { get; set; } =
+    [
         new RequiredFieldModel { FieldName = "Account Number" },
         new RequiredFieldModel { FieldName = "Service Type" },
         new RequiredFieldModel { FieldName = "Vehicle Type" },
-        new RequiredFieldModel { FieldName = "Package Type" }
-    };    
+        new PackageItemization { FieldName = "Package Type" }
+    ];    
     // Time Windows Properties
     public string PickupFromDateSource { get; set; } = "FromFile";
     public string PickupFromTimeSource { get; set; } = "FromFile";
@@ -25,9 +25,4 @@ public class IntegrationIntakeModel
     public string DeliveryFromTimeSource { get; set; } = "FromFile";
     public string DeliveryToDateSource { get; set; } = "FromFile";
     public string DeliveryToTimeSource { get; set; } = "FromFile";
-    
-    // Package Itemization Properties
-    public bool EnableItemization { get; set; } = false;
-    public NoItemizationType NoItemizationSubOption { get; set; } = NoItemizationType.FromFileWithMapping;
-    public string ItemizedRefNoMapping { get; set; } = string.Empty;
 }
